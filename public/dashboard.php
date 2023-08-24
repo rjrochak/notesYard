906d5779de793f358fbfed22e7dbc551ae45d29b
abhishek91999 <abhishekchoubey5544@gmail.com>
2023-08-24 15:23:58 +0530

    dashboard

---
+++ public/dashboard.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>dashboad admin</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    
+
+</head>
+<style>
+    * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    list-style-type: none;
+    text-decoration: none;
+    font-family: 'poppins',sans-serif;
+}
+
+.sidebar{
+    width: 345px;
+    left: 0;
+    top: 0;
+    height: 100%;
+    background-color: rgb(116, 78, 28);
+}
+.main-content {
+    margin-left: 345px;
+}
+</style>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="lab la-assignment"></span> ASSIGNMENT</h2>
+        </div>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href=""><span class="las la-igloo"></span>
+                    <span>Dashbord</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-user"></span>
+                    <span>PROFILE</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-igloo"></span>
+                    <span>EDIT</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-igloo"></span>
+                    <span>EDIT PROFILE PAGE</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-igloo"></span>
+                    <span>CLICK ON UPDATE</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-igloo"></span>
+                    <span>FILE UPDATE</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-igloo"></span>
+                    <span>SUCCESSFULLY</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <header>
+            <h2><div class="header-title">
+                <label for="">
+                    <span class="las la-bars"></span>
+                </label>
+                dashboard
+            
+            </h2>
+            <div class="search-warpper">
+                <span class="las la-search"></span>
+                <input type="search"placeholder="search here" />
+            </div>
+            <div class="user-wrapper">
+                <img src="img/2.jpg.jpeg" width="40px" hight="40px"alt="">
+            </div>
+            <h4>ABHISHEK</h4>
+            <small>admin</small>
+            </div>
+            </div>
+        </header>
+        <main>
+            <div class="cards">
+                <div class="card-single">
+                    ><div>
+                        <h1>1</h1>
+                        <span>PROFILE</span>
+                    </div>
+                    <div>
+                        <div>
+                            <span class="las la-edit"></span>
+                        </div>
+                    </div>
+                    <div class="card single">
+
+                    </div>
+                    <div>
+                    <h1>1</h1>
+                        <span>EDIT</span>
+                    </div>
+                    <div>
+                        <div>
+                            <span class="las la-edit"></span>
+                        </div>
+                    </div>
+                    <div class="card single">
+
+                    </div>
+                    </div>
+                    <div>
+                    <h1>1</h1>
+                        <span>EDIT PROFILE PAGE</span>
+                    </div>
+                    <div>
+                        <div>
+                            <span class="las la-edit"></span>
+                        </div>
+                    </div>
+                    <div class="card single">
+
+                    </div>
+                    </div>
+                    <div>
+                    <h1>1</h1>
+                        <span>CLICK ON UPDATE</span>
+                    </div>
+                    <div>
+                        <div>
+                            <span class="las la-edit"></span>
+                        </div>
+                    </div>
+                    <div class="card single">
+
+                    </div>
+                    </div>
+                    <div>
+                    <h1>1</h1>
+                        <span>FILE UPDATE</span>
+                    </div>
+                    <div>
+                        <div>
+                            <span class="las la-edit"></span>
+                        </div>
+                    </div>
+                    <div class="card single">
+
+                    </div>
+                    </div>
+                    <div>
+                    <h1>1</h1>
+                        <span>SUCCESSFULLY</span>
+                    </div>
+                    <div>
+                        <div>
+                            <span class="las la-success"></span>
+                        </div>
+                    </div>
+                    <div class="card single">
+
+                    </div>
+                    </div>
+                    <div>
+                    
+
+                    </div>
+                    </div>
+                   
+            </div>
+        </main>
+    </div>
+
+    
+</body>
+</html>
